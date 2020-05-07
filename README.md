f01025b1b2cd42ac120fb803fc0c9e7cdb67af95
Conrad Taylor <conradwt@gmail.com>
2020-05-06 21:19:52 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Rocket Reserver
+
+A simple getting started app using Apollo iOS Client.
+
+## Getting Started
+
+## Software requirements
+
+- Xcode 11.4.1 or higher
+
+- iOS 13.4.1 or higher
+
+- Swift 5.2 or higher
+
+## Navigate to the iOS application
+
+```
+$ cd /path/to/ios/application
+```
+
+## Starting the application
+
+```zsh
+open *.xcodeproj
+```
+
+Followed by
+
+```text
+Command + R
+```
+
+## References
+
+- https://www.apollographql.com/docs/ios/tutorial/tutorial-introduction
+
+## Support
+
+Bug reports and feature requests can be filed with the rest for Rocker Reserver project here:
+
+- [File Bug Reports and Features](https://github.com/conradwt/rocker-reserver/issues)
+
+## License
+
+Rocket Reserver is released under the [MIT license](https://mit-license.org).
+
+## Copyright
+
+copyright:: (c) Copyright 2020 Conrad Taylor. All Rights Reserved.
--- RocketReserver/MasterViewController.swift
+++ RocketReserver/MasterViewController.swift
@@ -7,20 +7,33 @@
 //
 
 import UIKit
+import Apollo
 
 class MasterViewController: UITableViewController {
 
   var detailViewController: DetailViewController? = nil
-  var objects = [Any]()
-
+  let apollo = ApolloClient(url: URL(string: "https://apollo-fullstack-tutorial.herokuapp.com/")!)
+  var launches = [LaunchListQuery.Data.Launch.Launch]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    self.apollo.fetch(query: LaunchListQuery()) { [weak self] result in
+      switch result {
+      case .success(let graphQLResult):
+        if let launches = graphQLResult.data?.launches.launches.compactMap({ $0 }) {
+          self?.launches.append(contentsOf: launches)
+          self?.tableView.reloadData()
+        }
+        print(graphQLResult)
+      case .failure(let error):
+        print(error)
+      }
+    }
+    
     // Do any additional setup after loading the view.
     navigationItem.leftBarButtonItem = editButtonItem
 
-    let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-    navigationItem.rightBarButtonItem = addButton
     if let split = splitViewController {
         let controllers = split.viewControllers
         detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -32,24 +45,17 @@ class MasterViewController: UITableViewController {
     super.viewWillAppear(animated)
   }
 
-  @objc
-  func insertNewObject(_ sender: Any) {
-    objects.insert(NSDate(), at: 0)
-    let indexPath = IndexPath(row: 0, section: 0)
-    tableView.insertRows(at: [indexPath], with: .automatic)
-  }
-
   // MARK: - Segues
 
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showDetail" {
         if let indexPath = tableView.indexPathForSelectedRow {
-            let object = objects[indexPath.row] as! NSDate
-            let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-            controller.navigationItem.leftItemsSupplementBackButton = true
-            detailViewController = controller
+//            let object = objects[indexPath.row] as! NSDate
+//            let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+//            controller.detailItem = object
+//            controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+//            controller.navigationItem.leftItemsSupplementBackButton = true
+//            detailViewController = controller
         }
     }
   }
@@ -61,30 +67,14 @@ class MasterViewController: UITableViewController {
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return launches.count
   }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    let object = objects[indexPath.row] as! NSDate
-    cell.textLabel!.text = object.description
+    let launch = launches[indexPath.row]
+    cell.textLabel!.text = launch.site
     return cell
   }
-
-  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-  }
-
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-        objects.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
-    } else if editingStyle == .insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-  }
-
-
+  
 }
-
